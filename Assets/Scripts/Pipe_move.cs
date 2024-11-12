cc61de7a4ea09dee8dba7a719ce68e30f4a2289e
TienMinhDuong <tmd31052005@gmail.com>
2024-11-12 15:24:22 +0700

    Test thu thoi

--- Assets/Scripts/Pipe_move.cs
+++ Assets/Scripts/Pipe_move.cs
@@ -9,12 +9,13 @@ public class Pipe_move : MonoBehaviour
     private float minLeft;
     private void Start()
     {
-        minLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x-1;
+        minLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1;
+        Debug.Log("Hello World");
     }
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
-        if(transform.position.x <= minLeft)
+        if (transform.position.x <= minLeft)
         {
             Destroy(gameObject);
         }
